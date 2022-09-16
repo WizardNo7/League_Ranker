58ae9fcc703e4dd3349c722b6bfa195b077a5b1b
Eduan Slabbert <edslabbert@gmail.com>
2022-09-16 13:20:33 +0200

    Fulfill first test requirements.

--- ranker/ranker.py
+++ ranker/ranker.py
@@ -4,8 +4,16 @@ CLI application that parses match result inputs
 and returns the rankings for the league.
 """
 
+from pathlib import Path
+
 
 def load_results_from_file(file_path):
+    results_file = Path(file_path)
+
+    if not results_file.exists():
+        raise FileNotFoundError(
+            f"The specified file [ {file_path} ] could not be found.")
+
     pass
 
 
