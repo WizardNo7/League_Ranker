1ca37947e2531e5220fcbe3506c8c38a2de09145
Eduan Slabbert <edslabbert@gmail.com>
2022-09-16 13:26:15 +0200

    Add test for successfully loaded file.

--- test/test_ranker.py
+++ test/test_ranker.py
@@ -15,3 +15,21 @@ def test_load_results_from_file_error():
     assert str(
         exc_info.value) == (f"The specified file [ {test_file_path} ] "
                             "could not be found.")
+
+
+def test_load_results_from_file(tmp_path):
+    example_results = [
+        "Lions 3, Snakes 3\n",
+        "Tarantulas 1, FC Awesome 0\n",
+        "Lions 1, FC Awesome 1\n",
+        "Tarantulas 3, Snakes 1\n",
+        "Lions 4, Grouches 0\n"
+    ]
+
+    test_file_path = tmp_path / "test"
+    test_file_path.mkdir()
+    test_file = test_file_path / "test.txt"
+    test_file.write_text("".join(example_results))
+
+    assert ranker.load_results_from_file(
+        test_file) == example_results
